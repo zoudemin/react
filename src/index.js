c1eded471dc18146ace8e148bb34ebb5615469d2
zoudemin <1522263113@qq.com>
2020-01-04 12:10:31 +0800

    delete

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
-import './index.less';
-
-ReactDOM.render(
-    <div>zoudemin<App/></div>,document.getElementById('app')
-)
