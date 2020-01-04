864af2af55cbc661581b176eef41d48948c216ab
zoudemin <1522263113@qq.com>
2020-01-04 11:01:02 +0800

    首次提交项目文件

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.less';
+
+ReactDOM.render(
+    <div>zoudemin<App/></div>,document.getElementById('app')
+)
